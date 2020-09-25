79eb4120a964eafa20ba4071510c51975c0fdc8f
bera5186 <rbasu611@gmail.com>
2020-09-26 00:56:12 +0530

    Trying React concurrent mode

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // pages
-import { Dashboard } from "./pages/dashboard.page";
-import  PricingPage  from "./pages/pricing.page";
+// import { Dashboard } from "./pages/dashboard.page";
+// import  PricingPage  from "./pages/pricing.page";
+const Dashboard = React.lazy(() => import("./pages/dashboard.page"));
+const PricingPage = React.lazy(() => import("./pages/pricing.page"));
 
 function App() {
-  return(
-    <Router>
-    <Switch>
-      <Route path="/dash">
-          <Dashboard />
-      </Route>
-      <Route path="/pricing">
-          <PricingPage />
-      </Route>
-    </Switch>
-  </Router>
+  return (
+    <React.Suspense fallback={<h3>Loading..</h3>}>
+      <Router>
+        <Switch>
+          <Route path="/dash">
+            <Dashboard />
+          </Route>
+          <Route path="/pricing">
+            <PricingPage />
+          </Route>
+        </Switch>
+      </Router>
+    </React.Suspense>
   );
-};
+}
 
 export default App;
